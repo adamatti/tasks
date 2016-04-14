44ed4b28ab5bdcc16ded1a04fbcd1185b13a65a5
Marcelo Adamatti <adamatti@gmail.com>
2016-04-14 15:34:05 -0300

    Added new relic

--- app/index.js
+++ app/index.js
@@ -5,6 +5,10 @@ const log4js = require("log4js"),
       models = require("./models")
 ;
 
+if (process.env.NEW_RELIC_LICENSE_KEY){
+    require("newrelic");
+}
+
 log4js.configure(config.log4js);
 
 require("./processors/CrudProcessor").register(models);
